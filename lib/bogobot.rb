f7fb9181fa2e264db15370d36747f19b8e8fbc34
Jon Fu <zh2fu@uwaterloo.ca>
2017-04-27 16:09:47 -0400

    Run the bot from the top directory using run.rb

--- lib/bogobot.rb
+++ lib/bogobot.rb
@@ -3,7 +3,7 @@ require 'yaml'
 Dir["#{File.dirname(__FILE__)}/bogobot/*.rb"].each { |file| require file }
 
 module BogoBot
-  Config = YAML.load_file('../data/config.yaml')
+  Config = YAML.load_file('data/config.yaml')
   Bot = Discordrb::Commands::CommandBot.new token: Config['token'],
                                              client_id: Config['client_id'], 
                                              prefix: '`'
