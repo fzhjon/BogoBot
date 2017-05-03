03480ba7c8bafb58e2c0765a7cdc954b20b9b416
Jon Fu <fu.jon97@gmail.com>
2017-05-02 22:50:27 -0400

    indenting

--- lib/bogobot/commands/stop.rb
+++ lib/bogobot/commands/stop.rb
@@ -7,7 +7,7 @@ module BogoBot
       command(:stop,
               usage:"`stop while the bot is playing audio.",
               description: 'Stops audio from playing.') do |event|
-            event.voice.stop_playing if event.voice.playing?
+        event.voice.stop_playing if event.voice.playing?
       end
       
       
