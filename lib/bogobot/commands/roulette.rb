92e1518c657220a5c02e28b760d525f36a6cdb08
Dane Thomas <danenthomas@hotmail.com>
2017-06-05 20:52:49 -0400

    Pretty much working
    Need to hide the database uri

--- lib/bogobot/commands/roulette.rb
+++ lib/bogobot/commands/roulette.rb
@@ -11,7 +11,7 @@ module BogoBot
               usage:"roulette <points>, where points is your bet",
               description: "Bet points for big gains!") do |event, bet|
 
-        client = Mongo::Client.new('mongodb://127.0.0.1:27017/test')
+        client = Mongo::Client.new('mongodb://127.0.0.1:27017/roulettedb')
         points = client[:points]
         player = event.user
 
@@ -27,25 +27,29 @@ module BogoBot
         doc = points.find(name: player.id).first
         new_total = doc["points"].to_i - result
         # Gamble calculations
-        chance = rand(0..10)
-        case chance
-        when chance <= 4
+        chance = rand(101)
+        if chance <= 40
           result = 0
           event.send_message("Wow! You lost your bet!")
-        when chance == 9 || chance == 8
-          result = 1.5 * result
-          event.send_message("Not bad... Ok gains!")
-        when chance == 10
-          result = 3 * result
-          event.send_message("BIG GAINS! 100% return on investment! Great Value!")
-        else
+        elsif chance <= 65
           event.send_message("Hmm, no gains...")
+        elsif chance <= 85
+          result = (1.5 * result).to_i
+          event.send_message("Not bad... Ok gains!")
+        elsif chance <= 95
+          result = 2 * result
+          event.send_message("BIG GAINS! 100% return on investment! Great Value!")
+        elsif chance <= 99
+          result = 10 * result
+          event.send_message("💵💵💵💵💸💸💸💰💰🤑💰💰💸💸💸💵💵💵💵")
+          event.send_message("You rich boi!!!")
+        elsif chance == 100
         end
-        new_total = new_total + result
-        event.send_message("New Point Total: #{ new_total }")
 
+        new_total = new_total + result
         # Update database
         points.update_one({ 'name' => player.id }, { '$set'=> { 'points' => new_total } } )
+        event.send_message("New Point Total: #{ new_total }")
 
       end
 
