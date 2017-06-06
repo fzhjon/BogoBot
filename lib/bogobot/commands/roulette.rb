8446a172f913de707978f5aa3acf15e6dfeeca02
Dane Thomas <danenthomas@hotmail.com>
2017-06-05 21:58:33 -0400

    Hidden database URI and can't bet what you don't have

--- lib/bogobot/commands/roulette.rb
+++ lib/bogobot/commands/roulette.rb
@@ -11,7 +11,7 @@ module BogoBot
               usage:"roulette <points>, where points is your bet",
               description: "Bet points for big gains!") do |event, bet|
 
-        client = Mongo::Client.new('mongodb://127.0.0.1:27017/roulettedb')
+        client = Mongo::Client.new(Config['POINTSDB'])
         points = client[:points]
         player = event.user
 
@@ -25,31 +25,40 @@ module BogoBot
         # Take the points they're betting
         result = bet.to_i
         doc = points.find(name: player.id).first
-        new_total = doc["points"].to_i - result
-        # Gamble calculations
-        chance = rand(101)
-        if chance <= 40
-          result = 0
-          event.send_message("Wow! You lost your bet!")
-        elsif chance <= 65
-          event.send_message("Hmm, no gains...")
-        elsif chance <= 85
-          result = (1.5 * result).to_i
-          event.send_message("Not bad... Ok gains!")
-        elsif chance <= 95
-          result = 2 * result
-          event.send_message("BIG GAINS! 100% return on investment! Great Value!")
-        elsif chance <= 99
-          result = 10 * result
-          event.send_message("💵💵💵💵💸💸💸💰💰🤑💰💰💸💸💸💵💵💵💵")
-          event.send_message("You rich boi!!!")
-        elsif chance == 100
-        end
+        new_total = doc["points"].to_i
 
-        new_total = new_total + result
-        # Update database
-        points.update_one({ 'name' => player.id }, { '$set'=> { 'points' => new_total } } )
-        event.send_message("New Point Total: #{ new_total }")
+        # Make sure users have enough funds.
+        if new_total < result
+          event.send_message("Nice try, but you're too much of a pleb for that bet...")
+        else
+          new_total = doc["points"].to_i - result
+          # Gamble calculations
+          chance = rand(101)
+          if chance <= 40
+            result = 0
+            event.send_message("Wow! You lost your bet!")
+          elsif chance <= 65
+            event.send_message("Hmm, no gains...")
+          elsif chance <= 85
+            result = (1.5 * result).to_i
+            event.send_message("Not bad... Ok gains!")
+          elsif chance <= 95
+            result = 2 * result
+            event.send_message("100% return on investment! Great Value!")
+          elsif chance <= 99
+            result = 3 * result
+            event.send_message("BIG GAINS! 💰🤑💰")
+          elsif chance == 100
+            result = 10 * result
+            event.send_message("💵💵💵💵💸💸💸💰💰🤑💰💰💸💸💸💵💵💵💵")
+            event.send_message("You rich boi!!!")
+          end
+
+          new_total = new_total + result
+          # Update database
+          points.update_one({ 'name' => player.id }, { '$set'=> { 'points' => new_total } } )
+          event.send_message("New Point Total: #{ new_total }")
+        end
 
       end
 
